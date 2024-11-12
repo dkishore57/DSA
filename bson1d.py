1707fdca7fe51711464c327a036d13ddac1810b6
dkishore <dkishorekumar57@gmail.com>
2024-11-12 14:41:49 +0530

    bson1d over

--- bson1d.py
+++ bson1d.py
@@ -112,19 +112,92 @@ class binarysearch:
                 ans=min(ans,arr[mid])
                 high=mid-1
         return ans
+    def min_in_rotated_sorted_array(self,arr):
+        low=0
+        high=len(arr)-1
+        ans=float('inf')
+        while(low<=high):
+            mid=(low+high)//2
+            if arr[low]<=arr[mid]:
+                ans=min(ans,arr[low])
+                low=mid+1
+            else:
+                ans=min(ans,arr[mid])
+                high=mid-1
+        return ans
+    def how_many_times_array_rotated(self,arr):
+        low=0
+        high=len(arr)-1
+        ans=float('inf')
+        ind=-1
+        while(low<=high):
+            mid=(low+high)//2
+            if arr[low]<=arr[mid]:
+                if ans<arr[low]:
+                    ans=arr[low]
+                    ind=low
+                low=mid+1
+            else:
+                if ans<arr[mid]:
+                    ans=arr[mid]
+                    ind=mid
+                high=mid-1
+        return ind
+    def single_element(self,arr):
+        if len(arr)==1:return arr[0]
+        if arr[0]!=arr[1]:return arr[0]
+        if arr[len(arr)-1]!=arr[len(arr)-2]:return arr[len(arr)-1]
+        low=1
+        high=len(arr)-2
+        while(low<=high):
+            mid=(low+high)//2
+            if arr[mid]!=arr[mid+1] and arr[mid]!=arr[mid-1]:
+                return arr[mid]
+            if(mid%2==1 and arr[mid]==arr[mid-1]) or (mid%2==0 and arr[mid]==arr[mid+1]):
+                low=mid+1
+            else:
+                high=mid-1
+        return -1
+    def peak_element(self,arr):
+        if len(arr)==1:
+            return arr[0]
+        if arr[0]>arr[1]:
+            return arr[0]
+        if arr[len(arr)-1]>arr[len(arr)-2]:
+            return arr[len(arr)-1]
+        low=1
+        high=len(arr)-2
+        while(low<=high):
+            mid=(low+high)//2
+            if(arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1]):
+                return arr[mid]
+            elif arr[mid]>arr[mid+1]:
+                high=mid-1
+            else:
+                low=mid+1
+        return -1
+
+
+
+
+
+        
+
 ''' Search insert position is same as lower bound
     ceil of the array is same as lower bound
     first occurence is same as lower bound
     last occurence is same as upper bound-1
+    count occurence is first occurence - last occurence + 1
 '''
+    
+
+    
     
 
 
 obj=binarysearch()
-bsearch=obj.bsearch([1,2,3,4,5],6)
-lower_bound=obj.lower_bound([1,3,5,7,9,11,45],4)
-upper_bound=obj.upper_bound([1,2,2,2,2,3,5,8,99,101],6)
-floor=obj.floor([1,2,4,5,6,6,6],10)
-findmin=obj.findmin([2,3,4,5,0,1])
-print(findmin)
+peak=obj.peak_element([1,2,5,1,0,2,3,5,1])
+print(peak)
+
+
 
