32ab11362be9bc8acd58503b2e3f7896ea3a055d
dkishore57 <dkishorekumar57@gmail.com>
2025-02-06 19:42:55 +0530

    update

--- sorting.py
+++ sorting.py
@@ -24,11 +24,37 @@ class sorting:
                 arr[j-1],arr[j]=arr[j],arr[j-1]
                 j-=1
         return arr
-                
+    def merge_sort(self,arr,low,high):
+        mid=(low+high)//2
+        if low>=high:
+            return arr
+        self.merge_sort(arr,low,mid)
+        self.merge_sort(arr,mid+1,high)
+        self.merge(arr,low,mid,high)
+    def merge(self,arr,low,mid,high):
+        left=low
+        right=mid+1
+        temp=[]
+        while(left<=mid and right<=high):
+            if arr[left]<=arr[right]:
+                temp.append(arr[left])
+                left+=1
+            else:
+                temp.append(arr[right])
+                right+=1
+        while(left<=mid):
+            temp.append(arr[left])
+            left+=1
+        while(right<=high):
+            temp.append(arr[right])
+            right+=1
+        for i in range(low,high+1):
+            arr[i]=temp[i-low]
 
+
+                
+arr=[3,1,10,0,8,86,45]
 
 obj=sorting()
-selection_sort=obj.selection_sort([3,1,10,0,8,86,45])
-bubble_sort=obj.bubble_sort([1,2,3])
-insertion_sort=obj.insertion_sort([3,1,10,0,8,86,45])
-print(insertion_sort)
+merge_sort=obj.merge_sort(arr,0,6)
+print(arr)
